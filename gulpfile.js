1226b41693ac7a74b4e889d04ef8f0016ebcd01d
Lawrence Chang <lchang@credera.com>
2017-05-24 10:51:03 -0500

    set up site template and gulp configs

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp      = require('gulp');
+var plumber   = require('gulp-plumber');
+var sass      = require('gulp-sass');
+var webserver = require('gulp-webserver');
+var opn       = require('opn');
+var inject    = require('gulp-inject');
+
+var sourcePaths = {
+  styles: ['./sass/**/*.scss']
+};
+
+var distPaths = {
+  styles: './css'
+};
+
+var server = {
+  host: 'localhost',
+  port: '9001'
+}
+
+gulp.task('sass', function () {
+  gulp.src(sourcePaths.styles)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest(distPaths.styles));
+});
+
+gulp.task('index', function () {
+    var target = gulp.src('./index.html');
+    var sources = gulp.src(['./js/**/*.js'], {read: false});
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      host:             server.host,
+      port:             server.port,
+      livereload:       true,
+      directoryListing: false
+    }));
+});
+
+gulp.task('openbrowser', function() {
+  opn('http://' + server.host + ':' + server.port, {app: ['google chrome']});
+});
+
+gulp.task('watch', function(){
+  gulp.watch(sourcePaths.styles, ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['build', 'index', 'webserver', 'watch', 'openbrowser']);
