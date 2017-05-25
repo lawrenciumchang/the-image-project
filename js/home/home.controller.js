294755ddef81e92dd915ac689a7c15baa541b55f
Lawrence Chang <lchang@credera.com>
2017-05-25 12:16:27 -0500

    set up reading images from cloud storage

--- js/home/home.controller.js
+++ js/home/home.controller.js
@@ -4,17 +4,31 @@ angular
     .controller('HomeController', HomeController);
 
 /* @ngInject */
-function HomeController($q, $state, $firebaseAuth) {
+function HomeController($q, $state, $firebaseAuth, $firebaseStorage) {
     var vm = this;
+
+    vm.images = [];
 
     var auth = $firebaseAuth();
 
     activate();
 
     function activate() {
-        var promises = [];
+        var promises = [getImages()];
         return $q.all(promises).then(function() {
 
+        });
+    }
+
+    function getImages() {
+        // Will need to loop through db here
+        
+        var fileName = 'macos-sierra.jpg';
+        var storageRef = firebase.storage().ref('/images/' + fileName);
+        var storage = $firebaseStorage(storageRef);
+
+        storage.$getMetadata().then(function(metadata) {
+            vm.images.push(metadata);
         });
     }
 
