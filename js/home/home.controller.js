2a228dd092fedbd74667f182a1637cfa048a6841
Lawrence Chang <lchang@credera.com>
2017-05-25 09:17:01 -0500

    added error handling on failed signout

--- js/home/home.controller.js
+++ js/home/home.controller.js
@@ -29,39 +29,45 @@ function HomeController($q, $state) {
 
     function signUp(signUpForm) {
         firebase.auth().createUserWithEmailAndPassword(signUpForm.email, signUpForm.password)
-        .then(function(user) {
-            user.updateProfile({
-                displayName: signUpForm.name
-            }).then(function() {
-                $state.reload();
-            }).catch(function(error) {
-                console.log(error);
+            .then(function(user) {
+                user.updateProfile({
+                    displayName: signUpForm.name
+                }).then(function() {
+                    $state.reload();
+                }).catch(function(error) {
+                    console.log(error.code, error.message);
+                });
+            })
+            .catch(function(error) {
+                // TODO: display form errors to user
+                console.log(error.code, error.message);
             });
-        })
-        .catch(function(error) {
-            // TODO: display form errors to user
-            console.log(error.code, error.message);
-        });
     }
 
     function logIn(logInForm) {
         firebase.auth().signInWithEmailAndPassword(logInForm.email, logInForm.password)
-        .then(function() {
-            $state.reload();
-        })
-        .catch(function(error) {
-            // TODO: display form errors to user
-            console.log(error.code, error.message);
-        });
+            .then(function() {
+                $state.reload();
+            })
+            .catch(function(error) {
+                // TODO: display form errors to user
+                console.log(error.code, error.message);
+            });
     }
 
     function logOut() {
-        firebase.auth().signOut();
-        $state.reload();
+        firebase.auth().signOut()
+            .then(function() {
+                $state.reload();
+            })
+            .catch(function(error) {
+                console.log(error.code, error.message);
+            });
     }
 
     firebase.auth().onAuthStateChanged(function(user) {
         vm.user = user;
+        // For testing -- to remove later.
         if (vm.user) {
             console.log('User ' + vm.user.email + ' is signed in.');
         } 
