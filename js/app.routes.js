5309a4b7118cb3175d528452148780e3a5499508
Lawrence Chang <lchang@credera.com>
2017-05-25 15:45:04 -0500

    added new views

--- js/app.routes.js
+++ js/app.routes.js
@@ -17,5 +17,28 @@ function Routes($stateProvider, $urlRouterProvider) {
             templateUrl: 'js/home/home.template.html',
             controller: 'HomeController',
             controllerAs: 'vm'
+        })
+        .state('user', {
+            abstract: true,
+            url: '/user',
+            template: '<ui-view/>'
+        })
+        .state('user.images', {
+            url: '/images',
+            templateUrl: 'js/user/images/user.images.template.html',
+            controller: 'UserImagesController',
+            controllerAs: 'vm'
+        })
+        .state('user.upload', {
+            url: '/upload',
+            templateUrl: 'js/user/upload/user.upload.template.html',
+            controller: 'UserUploadController',
+            controllerAs: 'vm'
+        })
+        .state('user.settings', {
+            url: '/settings',
+            templateUrl: 'js/user/settings/user.settings.template.html',
+            controller: 'UserSettingsController',
+            controllerAs: 'vm'
         });
 };
---
+++ js/user/upload/user.upload.controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular
+    .controller('UserUploadController', UserUploadController);
+
+/* @ngInject */
+function UserUploadController($q, $firebaseAuth) {
+    var vm = this;
+
+    var auth = $firebaseAuth();
+
+    activate();
+
+    function activate() {
+        var promises = [];
+        return $q.all(promises).then(function() {
+
+        });
+    }
+
+    auth.$onAuthStateChanged(function(user) {
+        vm.user = user;
+    });
+
+}
